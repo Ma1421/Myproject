6714a551124d793460fa1a4c4763121f55b3612c
Ma1421 <mai08061286@gmail.com>
2023-02-02 23:25:03 +0900

    投稿機能一覧表示

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,8 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
+        <title>Timeline</title>
+        <!-- Fonts 
+        -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
